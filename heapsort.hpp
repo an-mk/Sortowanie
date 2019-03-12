6bbfccc0b981ee1d981e724a0e8fd4e744f75ce6
05032MB <36550385+05032MB@users.noreply.github.com>
2019-03-12 22:18:47 +0100

    heapsort

---
+++ heapsort.hpp
@@ -0,0 +1,19 @@
+#ifndef _HEAPSORT_H_
+#define _HEAPSORT_H_
+
+namespace sort{
+
+template<typename It>
+void heapsort(It beg, It end);
+
+template<typename T>
+void heapsort(T c)
+{
+	heapsort(begin(c), end(c));
+}
+
+}
+
+#include "heapsort.cpp"
+
+#endif
